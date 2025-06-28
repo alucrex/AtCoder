e11fe55cadde515911c5b86e09d10536042a9b29
alucrex <alucrex@gmail.com>
2025-06-28 23:59:59 +0900

    ABC412

---
+++ ABC/412/d.cpp
@@ -0,0 +1,199 @@
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+using namespace chrono;
+#define rep(i,n) for(int i = 0 ; i < (n) ; i++)
+#define rep1(i,n) for(int i = 1 ; i <= (n) ; i++)
+#define rrep(i,n) for(int i = (n) - 1 ; i >= 0 ; i--)
+#define rrep1(i,n) for(int i = (n) ; i > 0 ; i--)
+using ll = int64_t;
+using P = pair<int, int>;
+using PL = pair<ll,ll>;
+using PD = pair<double,double>;
+using ld = long double;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+using vP = vector<P>;
+template<typename T> using v = vector<T>;
+template<typename T> using vv = vector<vector<T>>;
+template<typename T> using vvv = vector<vector<vector<T>>>;
+template<typename T> using p_q = priority_queue<T, v<T>, greater<T>>;
+const int INF = 1001001001;
+const int INTMAX = (1ll<<31)-1;
+const ll LINF = (1ll<<62);
+const ll LLMAX = (1ll<<62) + ((1ll<<62)-1);
+#define line cout << "================================" << endl;
+#define Yn(x) ((x) ? "Yes" : "No")
+#define yn(x) ((x) ? "yes" : "no")
+#define debug(x) cout << #x" : " << (x) << endl;
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define m_p(a, b) make_pair(a, b)
+#define set_time(x) steady_clock::time_point (x) = steady_clock::now()
+#define p_time(x) duration_cast<milliseconds>(x).count()
+#define show_time(s, e) cout << "実行時間" << duration_cast<milliseconds>(e-s).count() << "ms\n"
+template <typename T> bool chmin(T& a, T b){if(a>b){a=b;return 1;}return 0;}
+template <typename T> bool chmax(T& a, T b){if(a<b){a=b;return 1;}return 0;}
+ostream& operator<< (ostream& os, P a) {return os << a.first << " , " << a.second;}
+
+class alucrex{
+    public:
+    void vin(vi& a){for(auto& i:a)cin>>i;}
+    void vin(vl& a){for(auto& i:a)cin>>i;}
+    void vin(vP& a){for(auto& x:a)cin>>x.first>>x.second;}
+    void vin1(vi& a){rep1(i,a.size()-1)cin>>a[i];}
+    void vvin(vvi& a){for(auto& i:a)for(auto& j:i)cin>>j;}
+    void vvin(vvl& a){for(auto& i:a)for(auto& j:i)cin>>j;}
+    void vvdes(vvi& a){for(auto& i:a){for(auto& j:i){
+        cout << j << " ";}puts("");
+    }}
+    void vvdes(vvl& a){for(auto& i:a){for(auto& j:i){
+        cout << j << " ";}puts("");
+    }}
+    template<typename T>
+    void vvd(vv<T>& a){rep(i,a.size()){rep(j,a[i].size()){
+        cout << a[i][j] << " ";}cout<<endl;
+    }}
+    bool TLE(steady_clock::time_point s, int time = 9800){
+        steady_clock::time_point e = steady_clock::now();
+        if(duration_cast<milliseconds>(e - s).count() >= time){puts("TLE");return true;}
+        return false;
+    }
+    ll rui(ll n, int r, ll m = LLMAX) {
+        n %= m;
+        ll res = (r < 2 ? 1 : n);
+        ll cnt = 1;
+        if(r >= 2)for( ; cnt *2 <= r ; cnt *= 2) {
+            res *= res;
+            res %= m;
+        }
+        else cnt = 0;
+        for(int i = 0 ; i < (r - cnt) ; i++) {res *= n; res %= m;}
+        return res;
+    }
+    ll kai(ll n) { //O(N)
+        ll res = 1;
+        for(int i = 2 ; i <= n ; i++) res *= i;
+        return res;
+    }
+};
+const int MOD = 1000000007;
+
+
+void up(vi &tb, int p) {
+    int i = p;
+    for(;;i++) {
+        if(tb[i]) {
+            tb[i]--;
+            tb[i+1]++;
+            return;
+        }
+    }
+}
+
+void down(vi &tb, int p) {
+    int i = p;
+    for(;;i--) {
+        if(tb[i]) {
+            tb[i]--;
+            tb[i-1]++;
+            return;
+        }
+    }
+}
+
+int main(){
+    //set_time(__start__);
+    alucrex al;
+    int n, m; cin >> n >> m;
+    vi d(n, 0);
+    vi tb(n, 0);
+    rep(i, m) {
+        int a, b; cin >> a >> b;
+        a--, b--;
+        d[a]++;
+        d[b]++;
+    }
+    for(auto f: d) tb[f]++;
+    
+    // line;
+    // for(auto f: tb) cout << f << " "; puts("");
+
+    int cnt = 0;
+    // 0->1
+    // 1->2
+    rep(i, 2) {
+        cnt += tb[i]/2;
+        int mv = tb[i] - (tb[i]&1);
+        tb[i] -= mv;
+        tb[i+1] += mv;
+        if(tb[i]) {
+            cnt++;
+            tb[i]--;
+            up(tb, i);
+            tb[i+1]++;
+        }
+        // for(auto f: tb) cout << f << " "; debug(cnt);
+    }
+
+    // line;
+
+    for(int i = n-1 ; i >= 3 ; i--) {
+        cnt += tb[i]/2;
+        int mv = tb[i] - (tb[i]&1);
+        tb[i] -= mv;
+        tb[i-1] += mv;
+        // for(auto f: tb) cout << f << " "; debug(cnt);
+        if(tb[i]) {
+            cnt++;
+            tb[i]--;
+            down(tb, i);
+            tb[i-1]++;
+            // for(auto f: tb) cout << f << " "; debug(cnt);
+        }
+    }
+
+    rep(i, 2) {
+        cnt += tb[i]/2;
+        int mv = tb[i] - (tb[i]&1);
+        tb[i] -= mv;
+        tb[i+1] += mv;
+        if(tb[i]) {
+            cnt++;
+            tb[i]--;
+            up(tb, i);
+            tb[i+1]++;
+        }
+        // for(auto f: tb) cout << f << " "; debug(cnt);
+    }
+
+    for(int i = n-1 ; i >= 3 ; i--) {
+        cnt += tb[i]/2;
+        int mv = tb[i] - (tb[i]&1);
+        tb[i] -= mv;
+        tb[i-1] += mv;
+        // for(auto f: tb) cout << f << " "; debug(cnt);
+        if(tb[i]) {
+            cnt++;
+            tb[i]--;
+            down(tb, i);
+            tb[i-1]++;
+            // for(auto f: tb) cout << f << " "; debug(cnt);
+        }
+    }
+
+    rep(i, n) {
+        if(i == 2) continue;
+        if(tb[i]) {
+            debug(i);
+            debug(tb[i]);
+            while(true);
+        }
+    }
+    cout << cnt << endl;
+
+    //set_time(__end__);
+    //show_time(__start__ , __end__);
+}
